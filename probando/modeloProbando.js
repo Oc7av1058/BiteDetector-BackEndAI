9a8174f868d634fa5e7ea6c6fc18c34a07ef09b7
TOMAS AGUSTIN AVOLA <47297744@ort>
2023-06-30 12:01:06 -0300

    modelo hecho pero anda para el culo

--- probando/modeloProbando.js
+++ probando/modeloProbando.js
@@ -1,32 +1,74 @@
-import * as tf from '@tensorflow/tfjs';
-import * as qsy from 'tensorflow/tfjs-node';
-import {imagenes, inputHeight, inputWidth} from './listaImagenes';
-const epochs =10;
-const batchSize = 32 
+
+import * as tf from '@tensorflow/tfjs-node';
+import { imagenes, inputHeight, inputWidth } from './listaImagenes';
+const imageChannels = 1;
+const epochs = 10;
+const batchSize = 32
 //crea el modelo
 const model = tf.sequential();
-model.add(tf.layers.conv2d({/*aca no se que va */}));
+model.add(tf.layers.conv2d({
+  inputShape: [inputWidth, inputHeight, imageChannels],
+  filters: 8,
+  kernelSize: 5,
+  padding: 'same',
+  activation: 'relu'
+}));
+model.add(tf.layers.maxPooling2d({
+  poolSize: 2,
+  strides: 2
+}));
+model.add(tf.layers.conv2d({
+  filters: 16,
+  kernelSize: 5,
+  padding: 'same',
+  activation: 'relu'
+}));
+model.add(tf.layers.maxPooling2d({
+  poolSize: 3,
+  strides: 3
+}));
+model.add(tf.layers.flatten());
+model.add(tf.layers.dense({
+  units: numOfClasses,
+  activation: 'softmax'
+}));
+
+//lo compila
+model.compile({
+  optimizer: 'adam',
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy']
+});
+
+
 /*
  model.add(tf.layers.maxPooling2d({ ... }));  // Ejemplo de capa de agrupación
   model.add(tf.layers.flatten());
   model.add(tf.layers.dense({ ... }));  // Ejemplo de capa completamente conectada
   model.add(tf.layers.softmax());
 */
-//lo compila
-model.compile({
-    loss: 'categoricalCrossentropy',
-    optimizer: 'adam',
-    metrics: ['accuracy']
-  });
 
-  await model.fit(trainData, trainLabels, {
-    epochs: epochs,
-    batchSize: batchSize,
-    //validationData: [testData, testLabels],
-    callbacks: tf.node.tensorBoard('logs')
-  });
+//entrena el modelo
 
+const opciones = {
+  epochs: epochs,
+  verbose: 0,
+  callbacks: {
+    onEpochBegin: async (epoch, logs) => {
+      console.log(`Epoch ${epoch + 1} of ${epochs}`)
+    },
+    onEpochEnd: async (epoch, logs) => {
+      console.log(`  train-set loss: ${logs.loss.toFixed(4)}`)
+      console.log(`  train-set accuracy: ${logs.acc.toFixed(4)}`)
+
+    }
+  }
+}; 
+let info =model.fitDataset(imagenes.tensor, opciones);
+
+console.log('\r\n', info);
+console.log('\r\nEvaluating model...');
 //evalua la eficiencia del modelo
-  const evalResult = model.evaluate(testData, testLabels);
+const evalResult = model.evaluate(testData, testLabels);
 console.log('Test Loss:', evalResult[0].dataSync()[0]);
 console.log('Test Accuracy:', evalResult[1].dataSync()[0]);
